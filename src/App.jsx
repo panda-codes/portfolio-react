d2585cf27dcd165460822240f91313e05d41eab2
panda-codes <dedeomatse@gmail.com>
2025-08-31 23:21:00 +0100

    added package.txt file to keep track of the commands used to install important packages during this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+     <BrowserRouter>
+      <Routes>
+        <Route index element={<Home/>}/> {/*index just assummes that the path is the index page*/}
+        <Route path={"*"} element={<NotFound/>}/> 
+      </Routes>
+     </BrowserRouter> 
+    </>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      {/* theme toggle */}
+
+      {/* background effect */}
+    </div>
+  )
+}
+
+export default Home

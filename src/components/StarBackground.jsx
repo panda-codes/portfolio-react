4b4bf836ac4eaad6de601654774e441232a26f5e
panda-codes <dedeomatse@gmail.com>
2025-09-16 16:41:33 +0100

    starbackground component created

---
+++ src/components/StarBackground.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const StarBackground = () => {
+
+
+    //a star is an array of objects that have the following values;
+    // id, size, x, y, opacity, animationDuration
+
+    const [stars, setStars] = useState([]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StarBackground
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ThemeToggle from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
 
 const Home = () => {
   return (
@@ -8,7 +9,7 @@ const Home = () => {
       <ThemeToggle/>
 
       {/* BACKGROUND */}
-
+      <StarBackground/>
       {/* NAVBAR */}
 
       {/* MAIN CONTENT */}

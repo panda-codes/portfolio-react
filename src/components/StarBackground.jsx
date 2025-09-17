a40f16da36a4678c989ad004c5cc8467e8545b18
panda-codes <dedeomatse@gmail.com>
2025-09-17 14:02:39 +0100

    completed inline styling of stars being displayed in darkmode

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -35,8 +35,18 @@ const StarBackground = () => {
 
 
   return (
-    <div>
-      
+    <div className='fixed inset-0 overflow-hidden pointer-events-none z-0'>
+      {stars.map((star)=>(
+        <div key={star.id} className='star animate-pulse-subtle' style={{
+            width: star.size+"px",
+            height: star.size+"px",
+            left: star.x+"%",
+            top: star.y+"%",
+            opacity: star.opacity,
+            animationDuration: star.animationDuration+"s",
+
+        }}/>
+      ))}
     </div>
   )
 }

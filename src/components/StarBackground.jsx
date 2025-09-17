6596a1933ddd5c7cbf93e80d9bbeb800e976fb59
panda-codes <dedeomatse@gmail.com>
2025-09-17 11:01:14 +0100

    generate stars function has been created and useEffect hook is used to call the function as soon as the component loads

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const StarBackground = () => {
 
@@ -7,6 +7,32 @@ const StarBackground = () => {
     // id, size, x, y, opacity, animationDuration
 
     const [stars, setStars] = useState([]);
+
+    useEffect(()=>{
+        generateStars();
+    }, []);//This is immediately calling the generate stars function once the component loads
+
+    const generateStars=()=>{
+
+        const numberOfStars = Math.floor(
+            (window.innerWidth * window.innerHeight)/10000
+        )
+
+    const newStars = [];
+
+    for(let i=0; i < numberOfStars; i++){
+        newStars.push({
+            id:i,
+            size:Math.random() * 3 + 1,//this adds different sizes of stars between 1-4 px
+            x: Math.random() * 100,
+            y: Math.random() * 100,
+            opacity: Math.random() * 0.5 + 0.5,
+            animationDuration: Math.random() * 4 + 2,
+        })
+    }
+    setStars(newStars);
+    }
+
 
   return (
     <div>

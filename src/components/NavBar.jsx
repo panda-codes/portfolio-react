82c384a8fb8880f97254caa82a0805e5634094a3
panda-codes <dedeomatse@gmail.com>
2025-09-18 18:53:18 +0100

    done with logo part of the navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { href } from 'react-router-dom'
+import {cn} from '@/lib/utils'
 
 const navItems = [
-    {name:"Home",}
+    {name:"Home",href:"#hero"},
+    {name:"About",href:"#about"},
+    {name:"Skills",href:"#skills"},
+    {name:"Projects",href:"#projects"},
+    {name:"Content",href:"#content"},
+
 ]
 const NavBar = () => {
 
+const [isScrolled,setIsScrolled] = useState(false);
 
+useEffect(()=>{
+    const handleScroll =()=>{
+        setIsScrolled(window.scrollY > 10);
+    }
+
+    window.addEventListener("scroll",handleScroll);
+    return ()=> window.removeEventListener("scroll",handleScroll);
+
+}, [])
 
   return (
-    <div>
+    <nav className={cn("fixed w-full z-40 transistion-all duration-300",
+        isScrolled?"py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+    )}>
       
-    </div>
+      <div className='container flex items-center justify-between'>
+        <a className='text-xl font-bold text-primary flex items-center' 
+        href='#hero'>
+            <span className='relative z-10'>
+                <span className='text-glow text-foreground'>Dee'sTech</span> Portfolio
+            </span>
+        </a>
+      </div>
+    </nav>
   )
 }
 
